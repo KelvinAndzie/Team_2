708c20bf96954a20e953546217870a918c5dcd25
KelvinAndzie <kelvint.andzie@gmail.com>
2024-10-26 15:13:46 +0000

    logout

---
+++ register/login/login.js
@@ -0,0 +1,7 @@
+document.getElementById('login').addEventListener('click', function() {
+    window.location.href = '../../dashboard/dashboard.html';
+});
+
+document.getElementById('signup').addEventListener('click', function() {
+    window.location.href = '../signup/signup.html';
+});

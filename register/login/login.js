c157fa4aaa2e5cf3232dbd74a0c1c34445ee7b8d
KelvinAndzie <kelvint.andzie@gmail.com>
2024-10-27 16:41:31 +0000

    localStorage

--- register/login/login.js
+++ register/login/login.js
@@ -1,5 +1,21 @@
-document.getElementById('login').addEventListener('click', function() {
-    window.location.href = '../../dashboard/dashboard.html';
+document.getElementById('login').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
+
+    if(username === storedUsername && password === storedPassword) {
+        window.location.href = '../../dashboard/dashboard.html';
+    }
+    else{
+        alert("Invalid Username or Password!");
+        return;
+    }
+
+    
 });
 
 document.getElementById('signup').addEventListener('click', function() {
